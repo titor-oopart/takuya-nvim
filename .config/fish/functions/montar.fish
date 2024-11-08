4244bdb43244b17044b0f1ca7fe03bdf77fa747d
John Salazar Pinto <johnpiterzon@gmail.com>
2024-11-08 16:01:17 -0400

    fish montar alias added.

---
+++ .config/fish/functions/montar.fish
@@ -0,0 +1,4 @@
+function montar --wraps='sudo mount /dev/sda2 /media/titor/wd_02_12TB/' --description 'alias montar=sudo mount /dev/sda2 /media/titor/wd_02_12TB/'
+  sudo mount /dev/sda2 /media/titor/wd_02_12TB/ $argv
+        
+end
